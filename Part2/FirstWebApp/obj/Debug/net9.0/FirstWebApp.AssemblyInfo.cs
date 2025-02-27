70bc0dd594df36f26c8d04bd16643e3d921a7ae3
MortenStaunstrup <mort490fgromling@gmail.com>
2025-02-27 10:39:57 +0100

    Changes

--- Part2/FirstWebApp/obj/Debug/net9.0/FirstWebApp.AssemblyInfo.cs
+++ Part2/FirstWebApp/obj/Debug/net9.0/FirstWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ececac575d78c8d1446b4eaf3531472c934890fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7af7e3ca1bb47365f585db030347752d68d85452")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
